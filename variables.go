da7c87c93bcb71ff1f7dfafe0c2ae68eb1ae0edd
Bartosz Lachowicz <jendrekpb@gmail.com>
2022-10-08 13:44:01 +0200

    Playing with tutorials

---
+++ variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("ab")
+
+	// var a = "initial"
+    f := "apple"
+
+	fmt.Println(f)
+	f,x := "appleee", 123
+
+	fmt.Println(f)
+	fmt.Println(x)
+
+	// var b = 5
+}
